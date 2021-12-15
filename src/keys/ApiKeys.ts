d12065a121d04d3cc606169366400f0365e92e93
dell2duo <rafael.rocha.ufv@gmail.com>
2021-12-14 23:06:31 -0300

    Removing exposed keys

--- src/keys/ApiKeys.ts
+++
@@ -1,8 +0,0 @@
-/**
- * TODO: these should'nt be public, they should be in a key vault
- */
-
-export const keys = {
-  SPOTT_WEATHER_API_KEY: 'dc5cefc424mshc002bd6e10ed974p1553bejsndce0318bb3e2',
-  SPOTT_CITIES_API_KEY: 'dc5cefc424mshc002bd6e10ed974p1553bejsndce0318bb3e2',
-}
